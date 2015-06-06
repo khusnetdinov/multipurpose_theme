bc24fd31fd238c72f018d634065b88fef04f82e4
Marat Khusnetdinov <marat@khusnetdinov.ru>
2015-06-06 15:43:34 +0300

    Fix: generator

--- lib/generators/multipurpose_theme/install_generator.rb
+++ lib/generators/multipurpose_theme/install_generator.rb
@@ -1,16 +1,17 @@
 module MultipurposeTheme
   class InstallGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
+    
+    def assets
+      gem 'sass-rails', '~> 5.0'
+      gem 'bourbon'
+      gem 'jquery-rails'
 
-    gem 'sass-rails', '~> 5.0'
-    gem 'bourbon'
-    gem 'jquery-rails'
+      Bundler.with_clean_env { run 'bundle install' }
 
-    Bundler.with_clean_env { run 'bundle install' }
-
-    copy_file 'theme.css.scss', 'app/assets/stylesheets/theme.css.scss'
-    copy_file 'ie8.css.scss', 'app/assets/stylesheets/ie8.css.scss'
-    copy_file 'theme.js', 'app/assets/javascripts/theme.js'
-
+      copy_file 'theme.css.scss', 'app/assets/stylesheets/theme.css.scss'
+      copy_file 'ie8.css.scss', 'app/assets/stylesheets/ie8.css.scss'
+      copy_file 'theme.js', 'app/assets/javascripts/theme.js'
+    end
   end
 end
--- lib/multipurpose_theme.rb
+++ lib/multipurpose_theme.rb
@@ -2,10 +2,7 @@ require 'rails'
 require 'rails/generators'
 require 'bundler'
 require 'multipurpose_theme/version'
-
 require 'generators/multipurpose_theme/install_generator'
-
-
 
 module MultipurposeTheme
   class Engine < ::Rails::Engine
